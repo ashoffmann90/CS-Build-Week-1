35cc950fddf16bdace1235b65275d9c891053883
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-06-24 16:42:45 -0700

    Updates

---
+++ precache-manifest.ca25f5eeac2049dbd7e2cc6346da0e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649cb53ceaa32d369e30a6f16d61fbb4",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "01928205a4de333e5cc2",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.04f0dc49.chunk.css"
+  },
+  {
+    "revision": "30857da49811648201ec",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.d138da77.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.d138da77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01928205a4de333e5cc2",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.e88411c8.chunk.js"
+  },
+  {
+    "revision": "75be2a7f44a4408b4ec1",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.ac9945e6.js"
+  }
+]);
